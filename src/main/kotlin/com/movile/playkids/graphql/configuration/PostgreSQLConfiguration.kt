718ba7b29cf00129c6d3839b63852854447f23d0
Julio Moreira B. Barros <julio.barros@playkids.com>
2019-02-07 13:45:09 -0200

    config

---
+++ src/main/kotlin/com/movile/playkids/graphql/configuration/PostgreSQLConfiguration.kt
@@ -0,0 +1,8 @@
+package com.movile.playkids.graphql.configuration
+
+/**
+ * @author Júlio Moreira Blás de Barros (julio.barros@movile.com)
+ * @since 2/7/19
+ */
+class PostgreSQLConfiguration {
+}
